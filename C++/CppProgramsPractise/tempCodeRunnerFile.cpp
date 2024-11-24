26cade706850795f1fbf338bd58f2cb2c4179a9d
Al Hosaine Al Amin (Parvez Khan) <alamin365p@gmail.com>
2024-11-24 21:18:14 +0600

    My initial Codes

--- C++/CppProgramsPractise/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-
-    int tc=0;
-    while(tc++ > 5)
-    {
-        cout<<tc;
-    }
-    cout<<endl;
-
--- My SQL/FirstDBMS Program.sql
+++
@@ -1,8 +0,0 @@
-create database college;
-use college;
-
-create table student(
-id int primary key,
-name varchar(50),
-age int not null 
-);
